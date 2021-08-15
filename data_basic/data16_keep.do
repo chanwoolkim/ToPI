f9b9697bd5b07ab8c45785c5c50b156313e545d6
Wonwoo Choi <chanwoolkim@uchicago.edu>
2021-08-15 13:22:51 +0900

    Causal tree update and variable choice updates accordingly

--- data_basic/data16_keep.do
+++ data_basic/data16_keep.do
@@ -41,20 +41,59 @@ gen cc_price_relative=cc_payments_site*4.2/(income_site/12)
 label var cc_payments_site 	"Z_p Mean Price of Alternative childcare"
 label var cc_price_relative "Z_p Price of Alternative childcare Relative to Mean Income"
 label var income_site		"Z_ph Mean income in area (proxy of wages)"
-label var caregiver_home	"Z_n Potential Caregiver Available at Home"
+label var caregiver_ever 	"Z_n Potential Caregiver Available at Home"
+label var caregiver_n 		"Z_n N Potential Caregivers Available "
 
 gen alt=.
 replace alt=0 if center!=.
-replace alt=1 if center==1 & D==0
+replace alt=1 if center==1 & D==0 // D==0 is equivalent to doing center_ehs==0
+
+gen n=alt==0 & D==0
+
+*Check the coding of Caregiver, which should definitely impact n and it is not impacting anything
+*Check why alt is not affeced by price, wtf
+
+*reg ever_center R caregiver_ever		cc_price_relative 	//very significant**
+*reg main_center R caregiver_ever		cc_price_relative 	//very significant**
+*reg center_ehs	R caregiver_ever		cc_price_relative	//R, price very significant
+*reg center		R caregiver_ever		cc_price_relative	//R, price very significant
+*reg D			R caregiver_ever		cc_price_relative	//R, price very significant
+*reg alt		R caregiver_ever		cc_price_relative	//R, but not price
+*reg n			R caregiver_ever		cc_price_relative	//R, price very significant
+
+*Caregiver does not do anything, so we move to this-->
+*reg center_ehs	R 	cc_price_relative	//R, price very significant
+*reg center		R 	cc_price_relative	//R, price very significant
+reg D			R 	cc_price_relative	//R, price very significant
+reg alt			R 	cc_price_relative	//R, but not price
+reg n			R 	cc_price_relative	//R, price very significant
+
+
+/*trying different specification of alt centers to see if it works
+gen private_center=.
+replace private_center=0 if ever_center!=. & D!=.
+replace private_center=1 if ever_center==1 & D==0
+reg private_center R caregiver_ever cc_price_relative //NOP
+*/
+
+/*trying different specification of EHS centers to see if it works
+gen ehs_center=.
+replace ehs_center=0 if ever_center!=. & D!=.
+replace ehs_center=1 if ever_center==1 & D==1
+reg ehs_center R caregiver_ever cc_price_relative //super influenced by the prices!!
+*/
+
 
 *Create Minimal Datasets
 keep id R D alt program_type sitenum ///
 m_iq m_age sex poverty m_edu gestage bw black mf sibling twin ///
-caregiver_home cc_payments_site income_site cc_price_relative ppvt3y center_ehs ehs_months
+caregiver_ever cc_payments_site income_site cc_price_relative ppvt3y center_ehs ehs_months
 
 order id R D alt program_type sitenum ///
 m_iq m_age sex poverty m_edu gestage bw black mf sibling twin ///
-caregiver_home cc_payments_site income_site cc_price_relative ppvt3y
+caregiver_ever cc_payments_site income_site cc_price_relative ppvt3y
+
+
 
 preserve
 keep if program_type==1
@@ -80,6 +119,7 @@ preserve
 keep if program_type==3|program_type==1
 *gen center_ehsmmixed=center_ehs 							//to work on the pile code
 *gen ehsmixed_months=ehs_months 								//to work on the pile code 
+outsheet using ehsmixed_center-topi.csv, comma nolabel replace		//for Chanwool/Athey
 save ehs_mixed_center-topi, replace
 restore
 
@@ -106,6 +146,8 @@ drop _merge
 *Instruments
 merge 1:1 id using ihdp-instruments
 drop _merge
+rename caregiver_three caregiver_home
+
 
 *Create instruments
 egen cc_payments_site =mean(weekly_cc_pay),by(site)
@@ -126,19 +168,17 @@ replace alt=0 if center!=.
 replace alt=1 if center==1 & D==0
 
 keep id R D alt site pag ///
-m_iq m_age sex poverty m_edu bw black mf sibling twin  ///
+m_iq m_age sex poverty m_edu bw black mf gestage sibling twin  ///
 caregiver_home cc_payments_site income_site cc_price_relative ppvt3y  sb3y 
 
 order id R D alt site pag ///
-m_iq m_age sex poverty m_edu bw black mf sibling twin  ///
+m_iq m_age sex poverty m_edu bw black mf gestage sibling twin  ///
 caregiver_home cc_payments_site income_site cc_price_relative ppvt3y  sb3y 
 
 outsheet using ihdp-topi.csv, comma nolabel replace			//for Chanwool/Athey
 save ihdp-juan, replace								//for Juan: few variables
 save ihdp-topi, replace
 
-
-asdsd
 
 /*NEEDS FIXING:
 use "abc-topi.dta", clear
@@ -201,7 +241,7 @@ use "abc-renamed-items.dta", clear
 
 keep id R D home1_* home3_* norm_home_* video* sb* $covariates ///
 bw  norm_pari* norm_pase* norm_bayley_* cbcl*  poverty black ///
-hs H twin ww /*home3y6m_original*/ home_jbg_learning
+/*hs0 H twin ww home3y6m_original*/ home_jbg_learning
 
 merge 1:1 id using "abc-preschools.dta"
 
--- pile/causal_tree.R
+++ pile/causal_tree.R
@@ -5,6 +5,7 @@
 library(causalTree)
 library(ggplot2)
 library(tidyverse)
+library(xtable)
 
 data_dir <- "C:/Users/561CO/Downloads/"
 output_dir <- "C:/Users/561CO/Downloads/"
@@ -12,15 +13,57 @@ seed <- 9657
 
 covariate_names <- c("bw", "twin", "m_age", "m_edu", "sibling", "m_iq", "black", "sex", "gestage", "mf")
 
+# Manual prediction
+traverse_tree <- function(tree, input, ind) {
+  current_var <- tree %>% filter(index==ind) %>% select(var) %>% as.character()
+  
+  if (current_var=="<leaf>") {
+    val <- tree %>% filter(index==ind) %>% select(yval) %>% as.numeric()
+    return(val)
+  }
+  else {
+    current_val <- input %>% select(current_var) %>% as.numeric()
+    
+    if (is.na(current_val)) {
+      return(NA)
+    }
+    else {
+      formula <- paste0(current_val,
+                        tree %>% filter(index==ind) %>% select(ltemp) %>% as.character())
+      if (eval(parse(text=formula))) {
+        traverse_tree(tree, input, 2*ind)
+      }
+      else {
+        traverse_tree(tree, input, 2*ind+1)
+      }
+    }
+  }
+}
+
 causal_matrix <- function(program, output_var, program_name) {
   formula_ct <- paste(output_var, "~", paste(covariate_names, collapse=" + "))
-  causal_tree <- causalTree(formula_ct,
+  causal_tree_deep <- causalTree(formula_ct,
                             data=program,
                             treatment=program$R,
                             split.Rule="CT",
                             split.Honest=FALSE,
                             cv.option="CT",
                             cv.Honest=FALSE)
+  
+  if (output_var=="ppvt3y" | output_var=="sb3y") {
+    pdf(paste0(output_dir, program_name, "_", output_var, "_deep_plot.pdf"))
+    prp(causal_tree_deep, extra=0)
+    dev.off()
+  }
+  
+  causal_tree <- causalTree(formula_ct,
+                            data=program,
+                            treatment=program$R,
+                            split.Rule="CT",
+                            split.Honest=FALSE,
+                            cv.option="CT",
+                            cv.Honest=FALSE,
+                            minsize=25)
   
   if (output_var=="ppvt3y") {
     abc_input <- abc %>%
@@ -37,13 +80,36 @@ causal_matrix <- function(program, output_var, program_name) {
     dev.off()
   }
   
-  causal_estimate <- mean(predict(causal_tree))
-  causal_estimate_abc <- mean(predict(causal_tree, newdata=abc_input))
+  opcp <- causal_tree$cptable[, 1][which.min(causal_tree$cptable[, 4])]
+  optree <- prune(causal_tree, cp=opcp)
+  
+  if (output_var=="ppvt3y" | output_var=="sb3y") {
+    pdf(paste0(output_dir, program_name, "_", output_var, "_pruned_plot.pdf"))
+    prp(optree, extra=0)
+    dev.off()
+  }
+  
+  causal_estimate <- mean(predict(causal_tree, na.action=na.omit))
+  causal_estimate_abc <- mean(predict(causal_tree, newdata=abc_input, na.action=na.omit))
+  
+  tree <- data.frame(index=row.names(causal_tree$frame),
+                     var=causal_tree$frame$var,
+                     yval=causal_tree$frame$yval) %>%
+    cbind(labels(causal_tree, collapse=FALSE))
+  
+  predicted_abc <- c()
+  
+  for (i in 1:nrow(abc_input)) {
+    predicted_abc[i] = traverse_tree(tree, abc_input[i,], 1)
+  }
+  
+  causal_estimate_abc_manual <- mean(predicted_abc, na.rm=TRUE)
   
   output_row <- data.frame(program=program_name,
                            output_var=output_var,
                            pre_estimate=causal_estimate,
-                           abc_estimate=causal_estimate_abc)
+                           abc_estimate=causal_estimate_abc,
+                           abc_estimate_manual=causal_estimate_abc_manual)
   output_row
 }
 
@@ -57,12 +123,13 @@ abc <- read.csv(paste0(data_dir,"abc-topi.csv")) %>%
 output <- data.frame(program=NULL,
                      output_var=NULL,
                      pre_estimate=NULL,
-                     estimate=NULL)
+                     abc_estimate=NULL,
+                     abc_estimate_manual=NULL)
 
-ehscenter_output <- c("norm_home_learning3y", "norm_home_total3y", "ppvt3y") #, "home3y_original")
-ehsmixed_output <- c("norm_home_learning3y", "norm_home_total3y", "ppvt3y") #, "home3y_original")
-ihdp_output <- c("norm_home_learning3y", "norm_home_total3y", "home_jbg_learning", "ppvt3y", "sb3y") #, "home3y_original")
-abc_output <- c("norm_home_learning3y", "norm_home_total3y", "home_jbg_learning", "sb3y") #, "home3y_original")
+ehscenter_output <- c("ppvt3y") #, "norm_home_learning3y", "norm_home_total3y", "home3y_original")
+ehsmixed_output <- c("ppvt3y") #, "norm_home_learning3y", "norm_home_total3y", "home3y_original")
+ihdp_output <- c("ppvt3y", "sb3y") #, "norm_home_learning3y", "norm_home_total3y", "home_jbg_learning", "home3y_original")
+abc_output <- c("sb3y") #, "norm_home_learning3y", "norm_home_total3y", "home_jbg_learning", "home3y_original")
 
 for (v in ehscenter_output) {
   output <- rbind(output, causal_matrix(ehscenter, v, "ehscenter"))
@@ -76,4 +143,16 @@ for (v in ihdp_output) {
   output <- rbind(output, causal_matrix(ihdp, v, "ihdp"))
 }
 
-write.csv(output, paste0(data_dir, "causal_tree.csv"), row.names=FALSE)
+write.csv(output, paste0(output_dir, "causal_tree.csv"), row.names=FALSE)
+
+output <- output %>%
+  rename('Program'=program,
+         'Outcome'=output_var,
+         'Pre-Estimate'=pre_estimate,
+         'ABC-Estimate'=abc_estimate,
+         'ABC-EStimate-Manual'=abc_estimate_manual)
+
+print(xtable(output,digits=c(0, 0, 0, 3, 3, 3)),
+      include.rownames=FALSE,
+      comment=FALSE,
+      file=paste0(output_dir,"causal_forest_output.tex"))
